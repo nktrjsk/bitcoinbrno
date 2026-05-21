680f39a42fa330ae484304855195133a9df77a94
Nikita Rjaško <github@nktrjsk.cz>
2026-05-21 21:50:20 +0200

    feat: replace inline SVG logos with bitcoinbrno.jpg

--- app/components/app/Logo.vue
+++ app/components/app/Logo.vue
@@ -1,4 +1,3 @@
 <template>
-  <img src="/app/logo.svg" alt="Jednadvacet" class="h-6 dark:hidden">
-  <img src="/app/logo-dark.svg" alt="Jednadvacet" class="h-6 hidden dark:block">
+  <img src="/bitcoinbrno.jpg" alt="Bitcoin Brno" class="h-6">
 </template>
